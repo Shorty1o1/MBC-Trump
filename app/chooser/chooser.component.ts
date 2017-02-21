9bad1f5a5d256c91511981b520ebb97ad2a865f7
Arne Gueldener <arne.gueldener@haw-hamburg.de>
2017-02-21 22:53:08 +0100

    Songs erscheinen nun auch

--- app/chooser/chooser.component.ts
+++ app/chooser/chooser.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Http} from '@angular/http';
-var  selectedArtist;
+var selectedArtist;
+var selectedAlbum;
 
 @Component({
     selector: 'app-about',
@@ -11,23 +12,34 @@ export class ChooserComponent {
     welcome : string;
     list;
     
-    albums;
+    
     constructor(private http:Http){
         this.welcome = "Hier kann man bald Songs auswählen"
         this.http.get('app/chooser/artists.json').subscribe(res => this.list = res.json());        
     };
   
     setArtist(artist) {        
-        console.log("Artist selected: " + artist);
+        console.log("SetArtist: " + artist);
         selectedArtist = artist;
     }
-    
-    getArtist(): string {        
-        console.log("Get: " + selectedArtist);
+			
+    getArtist() : string {        
+        console.log("GetArtist: " + selectedArtist);
         return selectedArtist;
     }
+	
+	setAlbum(album) {
+		console.log("SetAlbum: " + album)
+		selectedAlbum = album;
+	}	
+    
+	getAlbum() : string {
+		console.log("Ge3tAlbum: " + selectedAlbum);
+        return selectedAlbum;
+	}
 
     generateArray(obj){
-       return Object.keys(obj).map((key)=>{ return obj[key]});
+		console.log("GenerateArray: " + obj);
+		return Object.keys(obj).map((key)=>{ return obj[key]});
     }
 };
