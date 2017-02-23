aa89636162b2a3eb97623fa82d943601b6076dc3
Arne Gueldener <arne.gueldener@haw-hamburg.de>
2017-02-23 16:41:10 +0100

    Songauswahl funktioniert nun, Filter auch, hinzufuegen und entfernen von songs aus playlist auch

--- app/chooser/chooser.component.ts
+++ app/chooser/chooser.component.ts
@@ -7,32 +7,63 @@ import {Http} from '@angular/http';
     styleUrls: ['app/chooser/panelStyle.css']
 })
 export class ChooserComponent {
-    welcome : string;
-    
+	private artists[] : Artist [];
 	private selectedArtist : Artist;
 	private selectedAlbum : Album;
 	private selectedSong : Song;
-
+	private newPlayList[] : Song[];
+	
+	// Filter muessen initialisiert werden, sonst ist liste am Anfang leer
+	artistFilter = ""; 
+	albumFilter = "";
+	songFilter = "";
 	
     constructor(private http:Http){
-        this.welcome = "Hier kann man bald Songs auswählen" 	
-		
 		this.http.get('app/chooser/artists.json').subscribe(res => this.artists = res.json() as Artist[]); 
-    };
+	};
   
     setArtist(artist : Artist) {        
         console.log("SetArtist: " + artist.name);
         this.selectedArtist = artist;
+		this.selectedAlbum = null; // Damit sich die Songliste leert (vom vorherigen Artist)
     }			
    
 	setAlbum(album : Album) {
 		console.log("SetAlbum: " + album.title)
 		this.selectedAlbum = album;
+		
 	}	
+	
+	removeSongFromPlaylist(song : Song){
+		console.log("remove")
+		for(var i = 0; i < this.newPlayList.length; i++){
+			console.log("i: " +i)
+			if(this.newPlayList[i] == song) { // bloss nicht ===
+				console.log("gefunden")
+				this.newPlayList.splice(i, 1);
+				break;
+			}
+		}
+	}
  
-    setSong(song : Song) {
-		console.log("SetSong: " + song.title)
-		this.selectedSong = song;
+    addSongToPlaylist(song : Song) {
+		// Todo:
+		this.newPlayList[this.newPlayList.length] = song;
+		console.log("Länge: " + this.artists.length)
+	}
+	
+	addAlbumToPlaylist(album : Album) {
+		for(var i = 0; i < album.songs.length; i++) {			
+			this.addSongToPlaylist(album.songs[i]);
+		}
+	}
+	
+	addArtistToPlaylist(artist : Artist){
+		console.log("Add artist"  +artist.albums.length)
+		for(var i = 0; i < artist.albums.length; i++) {
+			console.log("Artist: " + artist.name + " , Album: " + artist.albums[i].title)
+			this.addAlbumToPlaylist(artist.albums[i]);
+		}
 	}
 };
 
