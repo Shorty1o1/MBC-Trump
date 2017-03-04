cb067bd977ee6c06e362bdb95604fd832d9dda59
Wassilij Beaucamp <wassilij.beaucamp@haw-hamburg.de>
2017-03-04 15:02:21 +0100

    changes to wSocketServer to accept only one connection as a master

--- backend/wSocketServer.ts
+++ backend/wSocketServer.ts
@@ -2,8 +2,10 @@
 export class WSocketServer{
     callbackFunction : Function;
     connections = [];
-    constructor(httpServer){
+    numberOfConnections : number = 0;
+    constructor(httpServer, numberOfConnections : number){
         var WebSocketServer = require('websocket').server;
+        this.numberOfConnections = numberOfConnections;
         var server = new WebSocketServer({
             httpServer : httpServer,
             autoAcceptConnections : false
@@ -13,16 +15,37 @@ export class WSocketServer{
 
 
     handleRequest = (request) => {
-        var connection = request.accept('echo-protocol', request.origin);
-        this.connections.push(connection);
-        connection.on('message',(messageObj) => {
-            this.callbackFunction(messageObj, connection);
-        });
 
-        connection.on('close', () => {
-            var index = this.connections.indexOf(connection);
-            this.connections.splice(index, 1);
-        })
+        if(this.numberOfConnections == 0){
+            var connection = request.accept('echo-protocol', request.origin);
+            this.connections.push(connection);
+
+            connection.on('message',(messageObj) => {
+                this.callbackFunction(messageObj, connection);
+            });
+
+            connection.on('close', () => {
+                var index = this.connections.indexOf(connection);
+                this.connections.splice(index, 1);
+            })
+        }else if(this.numberOfConnections == 1){
+            if(this.connections.length == 1 ){
+                var connection = request.reject();
+            }else{
+                var connection = request.accept('echo-protocol', request.origin);
+                this.connections.push(connection);
+
+                connection.on('message',(messageObj) => {
+                    this.callbackFunction(messageObj, connection);
+                });
+
+                connection.on('close', () => {
+                    var index = this.connections.indexOf(connection);
+                    this.connections.splice(index, 1);
+                })
+            }
+        }
+
     }
 
     addTextMessageHandler(handler : Function){
